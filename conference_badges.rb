61f3238f476b3acf1eb5bb8cb63b6c56dd2108a2
Sam D <samidiaz3@gmail.com>
2021-01-27 03:56:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -32,8 +32,8 @@ def printer(name_array)
 
 #  puts assign_rooms(name_array).inspect
 
-  name_array.each do |name|
-    puts assign_rooms(name_array)
+  batch_badge_creator(name_array).each do |name|
+    puts name
   end
 
 end
