62a44114b661fa1cf79da12215133585282c9bc6
Sam D <samidiaz3@gmail.com>
2021-01-27 03:12:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,9 @@ def assign_rooms (speaker_array)
   room_new_array = []
   speaker_array.each_with_index { |speaker, room_num|
     puts "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    room_new_array << speaker_array
   }
-     room_new_array << speaker_array
 
-    returns room_new_array
+
+    room_new_array
 end
