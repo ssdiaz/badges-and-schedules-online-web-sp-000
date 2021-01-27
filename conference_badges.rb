f901c6c507a3d953f6d9e1b2e97c5ae9fd910060
Sam D <samidiaz3@gmail.com>
2021-01-27 03:54:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ end
 # Tells printer what to print
 def printer(name_array)
   name_array.each do |name|
-    puts batch_badge_creator (name_array)
+    puts batch_badge_creator(name_array)
   end
 
 #  puts batch_badge_creator(name_array).inspect
