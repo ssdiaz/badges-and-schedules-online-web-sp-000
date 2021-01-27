63c69c68be449f5a8b4c48cc9c6a79b295398015
Sam D <samidiaz3@gmail.com>
2021-01-27 04:17:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,15 +4,15 @@ def badge_maker(name)
 end
 
 # Returns array of badge messages
-  # MY FIRST TRY:
-    #  def batch_badge_creator(name_array)
-    #    badge_new_array=[]
-    #    name_array.each do |name|
-    #      badge_new_array << badge_maker(name)
-    #    end
-    #      badge_new_array
-    #  end
-#SECOND TRY:
+    # MY FIRST TRY:
+      #  def batch_badge_creator(name_array)
+      #    badge_new_array=[]
+      #    name_array.each do |name|
+      #      badge_new_array << badge_maker(name)
+      #    end
+      #      badge_new_array
+      #  end
+  #SECOND TRY:
 def batch_badge_creator(speaker_list)
   speaker_list.map do |name|
     badge_maker(name)
@@ -20,26 +20,26 @@ def batch_badge_creator(speaker_list)
 end
 
 # Takes list of speaker and assigns a room number; returns an array
-  # MY FIRST TRY
-    #  def assign_rooms(name_array)
-    #    room_new_array = []
-    #    name_array.each_with_index { |name, room_num|
-    #        room_num = room_num + 1
-    #        room_new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
-    #    }
-    #      room_new_array
-    #  end
-#SECOND TRY
+    # MY FIRST TRY
+      #  def assign_rooms(name_array)
+      #    room_new_array = []
+      #    name_array.each_with_index { |name, room_num|
+      #        room_num = room_num + 1
+      #        room_new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
+      #    }
+      #      room_new_array
+      #  end
+  #SECOND TRY
 def assign_rooms(speaker_list)
     speaker_list.each_with_index.map do |name, room_num|
-      "Hello, #{name}! You'll be assigned to room #{room_num+1}!"
+      "Hello, #{name}! You'll be assigned to room #{room_num + 1}!"
   end
 end
 
 # Prints out both arrays above
 def printer(speaker_list)
-  batch_badge_creator(speaker_list).each do |badge|
-    puts badge
+  batch_badge_creator(speaker_list).each do |badge_message|
+    puts badge_message
   end
 
   assign_rooms(speaker_list).each do |room_assignment|
