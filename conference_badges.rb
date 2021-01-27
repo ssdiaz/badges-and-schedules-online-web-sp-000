2cfba7a4930aa39073565319d35dcd23503dd154
Sam D <samidiaz3@gmail.com>
2021-01-27 03:02:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+# Badge Message for name
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+# Creates Badge Message for array of names
+def batch_badge_creator (name_array)
+  badge_new_array=[]
+  name_array.each do |name|
+    badge_new_array << badge_maker(name)
+  end
+    badge_new_array
+end
+
+# Takes list of speaker and assigns a room
+def assign_rooms (speaker_array)
+# 7 rooms
+  room_new_array = []
+  speaker_array.each_with_index { |speaker, room_num|
+    puts "Hello, #{speaker}! You'll be assigned to room #{room_num}!
+  }
+    room_new_array << speaker_array
+  end
+end
