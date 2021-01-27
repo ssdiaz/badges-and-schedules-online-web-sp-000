fd03294c40f62c974bd914ab1e03f7a71ee2b348
Sam D <samidiaz3@gmail.com>
2021-01-27 04:16:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,14 +4,14 @@ def badge_maker(name)
 end
 
 # Returns array of badge messages
-  #FIRST TRY:
-  #  def batch_badge_creator(name_array)
-  #    badge_new_array=[]
-  #    name_array.each do |name|
-  #      badge_new_array << badge_maker(name)
-  #    end
-  #      badge_new_array
-  #  end
+  # MY FIRST TRY:
+    #  def batch_badge_creator(name_array)
+    #    badge_new_array=[]
+    #    name_array.each do |name|
+    #      badge_new_array << badge_maker(name)
+    #    end
+    #      badge_new_array
+    #  end
 #SECOND TRY:
 def batch_badge_creator(speaker_list)
   speaker_list.map do |name|
@@ -20,36 +20,29 @@ def batch_badge_creator(speaker_list)
 end
 
 # Takes list of speaker and assigns a room number; returns an array
-#FIRST TRY
-  #  def assign_rooms(name_array)
-  #    room_new_array = []
-  #    name_array.each_with_index { |name, room_num|
-  #        room_num = room_num + 1
-  #        room_new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
-  #    }
-  #      room_new_array
-  #  end
+  # MY FIRST TRY
+    #  def assign_rooms(name_array)
+    #    room_new_array = []
+    #    name_array.each_with_index { |name, room_num|
+    #        room_num = room_num + 1
+    #        room_new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    #    }
+    #      room_new_array
+    #  end
 #SECOND TRY
 def assign_rooms(speaker_list)
-  #speaker_list.map do |name|
     speaker_list.each_with_index.map do |name, room_num|
-    #  room_num = room_num + 1
       "Hello, #{name}! You'll be assigned to room #{room_num+1}!"
-
   end
 end
 
-
-
-
-
 # Prints out both arrays above
 def printer(speaker_list)
-  batch_badge_creator(speaker_list).each do |name|
-    puts name
+  batch_badge_creator(speaker_list).each do |badge|
+    puts badge
   end
 
-  assign_rooms(speaker_list).each do |name|
-    puts name
+  assign_rooms(speaker_list).each do |room_assignment|
+    puts room_assignment
   end
 end
